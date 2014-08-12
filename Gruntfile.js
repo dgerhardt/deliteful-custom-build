691cf703d72afe7417e20a7283b76d4233081110
Daniel Gerhardt <code@dgerhardt.net>
2014-08-12 23:40:00 +0200

    Fix parameters for amdserialize task invocation

--- Gruntfile.js
+++ Gruntfile.js
@@ -91,7 +91,7 @@ module.exports = function (grunt) {
 
 		layers.forEach(function (layer) {
 			grunt.task.run("amddepsscan:" + layer.name + ":" + name + ":" + amdloader);
-			grunt.task.run("amdserialize:" + layer.name + ":" + name + ":" + outprop);
+			grunt.task.run("amdserialize:" + layer.name + ":" + name + ":" + amdloader + ":" + outprop);
 			grunt.task.run("concat");
 			grunt.task.run("copy");
 		});
